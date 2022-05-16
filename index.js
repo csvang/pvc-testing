3ec3e61f8bd3e18140d4e228c26e381f6fc0c41e
Chue Shee Vang <csvang@gmail.com>
2022-05-16 17:56:05 -0500

    Implemented screenshots.

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ function pause(ms) {
 
 (async () => {
     try {
+        // const ss = 'ss_';
         const browser = await chromium.launch({
             args: ['--use-fake-ui-for-media-stream', '--use-fake-device-for-media-stream'], //https://github.com/microsoft/playwright/issues/2525
             //firefoxUserPrefs: { "media.navigator.streams.fake": true, "media.navigator.permission.disabled": true }, // for firefox
@@ -18,13 +19,15 @@ function pause(ms) {
         ctx.grantPermissions(['camera', 'microphone']);
         const page = await ctx.newPage();
 
-        //await page.goto(`file:///${process.cwd()}/prebuilt_demo_prejoin.html`);
+        //await page.goto(`file:///${process.cwd()/prebuilt_demo_prejoin.html`);
         await page.goto(`http://localhost:8000/prebuilt_demo_prejoin.html`);
 
-        await page.pause();
+        // await page.pause();
         await page.waitForSelector('#ready-room-root');
         // await page.waitForLoadState()
         if (await page.$('#ready-room-root > div > sw-prejoin > div > div > div.text-center > h1')) { // Show Prejoin H1
+            console.log(process.cwd());
+            await page.screenshot({ path: `${process.cwd()}/screenshots/ss-01_prejoin.png`, fullPage: true });
             // Camera
             await page.locator('text=SquareCheckboxAllow camera access').click();
             await pause(500);
@@ -46,6 +49,8 @@ function pause(ms) {
 
             // Audio Selection
             if (await page.$('text=Select your microphone and speakers')) {
+
+                await page.screenshot({ path: `${process.cwd()}/screenshots/ss-02_devices.png`, fullPage: true });
                 // audio = true;
                 await page.locator('text=Test Audio').click();
                 await pause(3000);
@@ -59,28 +64,33 @@ function pause(ms) {
 
         await page.waitForSelector('#ready-room-video-root');
 
+        await page.screenshot({ path: `${process.cwd()}/screenshots/ss-03_joined-video.png`, fullPage: true });
         if (await page.$('video')) {
             await page.locator('#ready-room-video-root').hover();
             await pause(500);
             await page.waitForSelector('top-bar button:has-text("People")');
             await page.locator('top-bar button:has-text("People")').click();
             await pause(500);
+            await page.screenshot({ path: `${process.cwd()}/screenshots/ss-04_people.png`, fullPage: true });
             await page.locator('button:has-text("Close")').click();
 
             const audioExist = page.$$('bottom-bar button:has-text("Mic")');
             if (audioExist) {
                 await page.locator('bottom-bar button:has-text("Mic")').click();
-            } 
+            }
 
             const videoExist = await page.$$('bottom-bar button:has-text("Videocam")');
             if (videoExist) {
                 await page.locator('bottom-bar button:has-text("Videocam")').click();
                 await pause(500);
                 await page.locator('bottom-bar button:has-text("Videocam")').click();
-            } 
+            }
 
+            await page.screenshot({ path: `${process.cwd()}/screenshots/ss-05_in-video.png`, fullPage: true });
             await pause(5000);
             await page.locator('button:has-text("Leave")').click();
+
+            await page.screenshot({ path: `${process.cwd()}/screenshots/ss-06_leave.png`, fullPage: true });
         }
 
         await browser.close();
